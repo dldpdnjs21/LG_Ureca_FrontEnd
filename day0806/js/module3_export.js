9f72b11e37f5982333f33deba8c7b68f5206ccac
Yewon_Lee <65122491+dldpdnjs21@users.noreply.github.com>
2024-08-06 14:50:10 +0900

    mdn_import_export

---
+++ day0806/js/module3_export.js
@@ -0,0 +1,6 @@
+const title='URECA';
+function add(su1,su2){
+    return su1+su2;
+}
+
+export{title,add};
---
+++ day0806/js/module3_import.js
@@ -0,0 +1,3 @@
+import {title,add} from './module2_export.js';
+console.log(add(10,2));
+console.log(title);
